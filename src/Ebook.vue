fd3270721ec655d5a94800b392c8a0ae16e71401
wuchuheng <wuchuheng@163.com>
2020-05-06 22:31:45 +0800

    【feature】电子书的解析和渲染

---
+++ src/Ebook.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ebook">
+    <div class="read-wrapper">
+      <div id="reader"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
+export default {
+  methods: {
+    showEpub() {
+      this.book = new Epub(DOWNLOAD_URL)
+      this.rendition = this.book.renderTo('reader', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      this.rendition.display()
+    }
+  },
+  mounted() {
+    this.showEpub()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '/assets/styles/global';
+</style>
